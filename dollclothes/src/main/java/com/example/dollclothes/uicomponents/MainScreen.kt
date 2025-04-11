2cf15f84af843acc585499c62dd94f67a2f8cc98
[nonameS0313] <nmj6451s@gmail.com>
2025-04-11 18:48:28 +0900

    file edit

--- dollclothes/src/main/java/com/example/dollclothes/uicomponents/MainScreen.kt
+++ dollclothes/src/main/java/com/example/dollclothes/uicomponents/MainScreen.kt
@@ -2,15 +2,18 @@ package com.example.dollclothes.uicomponents
 
 import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.dollclothes.model.DollViewModel
 import com.example.dollclothes.model.OnClothFactory
@@ -21,13 +24,16 @@ fun MainScreen(modifier: Modifier = Modifier, dollViewModel: DollViewModel = vie
     val doll = dollViewModel.dollList
 
     if (orientation == Configuration.ORIENTATION_PORTRAIT) {
-        Column(
-            modifier = Modifier.fillMaxHeight().fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.SpaceEvenly
-        ) {
-            DollWithClothes(doll)
-            CheckBoxTab(doll)
+        Box() {
+            Text("202111282 노을영", fontSize = 20.sp)
+            Column(
+                modifier = Modifier.fillMaxHeight().fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.SpaceEvenly
+            ) {
+                DollWithClothes(doll)
+                CheckBoxTab(doll)
+            }
         }
     } else {
         Row(
